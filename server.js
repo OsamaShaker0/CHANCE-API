66bc0513dc0fb6a561a46606308f1072379b84c8
osama shaker <152613379+OsamaShaker0@users.noreply.github.com>
2025-02-03 15:56:20 +0200

    add security package

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const fileUpload = require('express-fileupload');
 const express = require('express');
 const connectDB = require('./db/connectDB');
 const notFound = require('./middlewares/notFound');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const expressLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const hpp = require('hpp');
+const { xss } = require('express-xss-sanitizer');
 // routes
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
@@ -11,6 +18,14 @@ const errorHandler = require('./middlewares/error');
 const app = express();
 
 app.use(express.json());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(cors());
+app.use(hpp());
+app.use(cookieParser());
+const limiter = expressLimit({ windowMs: 10 * 60 * 1000, max: 100 });
+app.use(limiter);
+app.use(xss());
 app.use(fileUpload());
 
 app.use('/api/v1/users/auth', authRoute);
