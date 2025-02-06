fc2b5d17c3930aedfa423f81d104950719153150
osama shaker <152613379+OsamaShaker0@users.noreply.github.com>
2025-02-06 17:30:20 +0200

    refactor protect code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,27 +5,32 @@ const jwt = require('jsonwebtoken');
 
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
-  if (req.headers.authorization&&req.headers.authorization.startsWith('Bearer ')) {
+  // Check if the Authorization header exists and starts with 'Bearer '
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
     token = req.headers.authorization.split(' ')[1];
   }
   if (!token) {
-    next(new ErrorResponse(`Not authrized to access this route `, 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select('-password');
-
+    if (!req.user) {
+      return next(new ErrorResponse('User not found', 404));
+    }
     next();
   } catch (error) {
-    next(new ErrorResponse(`Not authrized to access this route `, 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 exports.authorize = (...roles) => {
   return (req, res, next) => {
-
     if (!roles.includes(req.user.permission)) {
-      throw new ErrorResponse(` Not authorized to access this route`, 403);
+      throw new ErrorResponse(`Not authorized to access this route`, 403);
     }
-    next()
+    next();
   };
-}
+};
